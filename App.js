bd3972b8bae657f9c3621d973511a9d94a0bde98
ArunaTebel <arunatebel@hotmail.com>
2021-07-04 15:42:36 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import type {Node} from 'react';
+import {Text} from 'react-native';
+
+const App: () => Node = () => {
+  return <Text>Welcome to archtodo!</Text>;
+};
+
+export default App;
