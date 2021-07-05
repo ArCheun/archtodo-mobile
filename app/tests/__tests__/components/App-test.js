5101e10f1af7c4437cb62b3311ec4f1ceff493c0
ArunaTebel <arunatebel@hotmail.com>
2021-07-05 16:07:49 +0530

    added component tests

--- app/tests/__tests__/components/App-test.js
+++ app/tests/__tests__/components/App-test.js
@@ -1,18 +1,18 @@
 import 'react-native';
 import React from 'react';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
 import App from '../../../App';
-import renderer, {act} from 'react-test-renderer';
 
 jest.mock('../../../utils/http');
-jest.useFakeTimers();
 
-describe('App component', () => {
-
-  it('renders without issues', () => {
-    act(() => {
-      renderer.create(<App />);
-    });
-  });
-
+test('Component will render without issues', async () => {
+  const {getAllByText} = await waitFor(() => render(<App />));
+  const todos = getAllByText(new RegExp(/Todo\s\d/, 'i'));
+  expect(todos).toHaveLength(5);
 });
 
+test('Todos will have correct texts upon rendering', async () => {
+  const {getAllByText} = await waitFor(() => render(<App />));
+  const todo3 = getAllByText('Todo 3');
+  expect(todo3).toHaveLength(1);
+});
